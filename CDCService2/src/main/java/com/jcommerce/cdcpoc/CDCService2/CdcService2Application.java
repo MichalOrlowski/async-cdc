3f09e46f545e0fea1221eee17a9dc21c46c81819
JCOMMERCE\prmi <michal.prajs@jcommerce.pl>
2017-06-12 12:03:10 +0200

    Consumer part

--- CDCService2/src/main/java/com/jcommerce/cdcpoc/CDCService2/CdcService2Application.java
+++ CDCService2/src/main/java/com/jcommerce/cdcpoc/CDCService2/CdcService2Application.java
@@ -3,10 +3,16 @@ package com.jcommerce.cdcpoc.CDCService2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.web.client.RestTemplate;
 
+import com.jcommerce.cdcpoc.CDCService2.model.TestModelObject;
+
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class CdcService2Application {
 
 
@@ -17,6 +23,11 @@ public class CdcService2Application {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
+	}
+	
+	@ServiceActivator(inputChannel=Sink.INPUT)
+	public void processTMO(TestModelObject toProcess) {
+		System.out.println(toProcess);
 	}
 
 //	@Bean
